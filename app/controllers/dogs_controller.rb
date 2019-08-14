490c796ba5f99f56011372969efab2be0f0e14cf
marshallhelenm <marshallhelenm@gmail.com>
2019-08-14 11:31:22 -0700

    dogs, yo

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,18 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        grab_dog
+        @employees = @dog.employees
+    end
+
+
+    private
+
+    def grab_dog
+        @dog = Dog.find(params[:id])
+    end
 end
